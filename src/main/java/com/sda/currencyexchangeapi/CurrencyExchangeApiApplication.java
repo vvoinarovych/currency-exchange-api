2f99af7f9691549175fc70a7feaa3c657e48aaec
Walerij <vvoinarovych@gmail.com>
2022-04-12 10:00:05 +0200

    initial commit

---
+++ src/main/java/com/sda/currencyexchangeapi/CurrencyExchangeApiApplication.java
@@ -0,0 +1,13 @@
+package com.sda.currencyexchangeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeApiApplication.class, args);
+    }
+
+}
